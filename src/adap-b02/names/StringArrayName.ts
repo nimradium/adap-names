79c2e366f0efee70cad26fe068c2c56c93facc4b
nimradium <29338598+nimradium@users.noreply.github.com>
2024-11-02 22:40:58 +0100

    b02: implemented StringName, StringArrayName

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -6,51 +6,55 @@ export class StringArrayName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation");
+        this.components = other;
+        if(delimiter != undefined) this.delimiter = delimiter;
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        return this.components.map((c) => c.replaceAll(ESCAPE_CHARACTER,'')).join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        if(this.delimiter === DEFAULT_DELIMITER) return this.components.join(DEFAULT_DELIMITER);
+        return this.components.map((c) => c.replaceAll(DEFAULT_DELIMITER, ESCAPE_CHARACTER + DEFAULT_DELIMITER).replaceAll(ESCAPE_CHARACTER + this.delimiter, this.delimiter)).join(DEFAULT_DELIMITER);
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.components.length === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation");
+        return this.components[i];
     }
 
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components[i] = c;
     }
 
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation");
+        this.components.splice(i, 0, c);
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation");
+        this.components.splice(i, 1);
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        if(this.delimiter !== other.getDelimiterCharacter()) return // https://www.studon.fau.de/studon/ilias.php?ref_id=4447999&cmdClass=ilobjforumgui&pos_pk=1130228&thr_pk=385173&page=0&viewmode=2&cmd=markPostRead&cmdNode=13z:tp&baseClass=ilRepositoryGUI#1130228
+        for(let i = 0; i < other.getNoComponents(); i++){
+            this.append(other.getComponent(i));
+        }
     }
-
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -8,51 +8,171 @@ export class StringName implements Name {
     protected length: number = 0;
 
     constructor(other: string, delimiter?: string) {
-        throw new Error("needs implementation");
+        this.name = other;
+        if(delimiter != undefined) this.delimiter = delimiter;
+
+        this.length = 1; // laut Forum: leerer String => eine Komponente https://www.studon.fau.de/studon/ilias.php?ref_id=4447999&cmdClass=ilobjforumgui&thr_pk=385940&page=0&cmd=viewThread&cmdNode=13z:tp&baseClass=ilRepositoryGUI
+        for(let i = 0; i < this.name.length; i++){
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                this.length++;
+            }
+        }
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
+        let out: string = this.replaceDelimiter(delimiter);
+        return out.replaceAll(ESCAPE_CHARACTER, '');
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation");
+        let out: string = this.replaceDelimiter(DEFAULT_DELIMITER);
+        return out;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation");
+        return this.name.length === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation");
+        return this.delimiter;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.length;
     }
 
     public getComponent(x: number): string {
-        throw new Error("needs implementation");
+        let ci = 0;
+        let i_start = 0; // incl.
+        let i_end = this.name.length;  // excl.
+        for(let i = 0; i < this.name.length; i++){
+            if(ci === x){
+                i_start = i;
+                break;
+            }
+
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                ci++;
+            }
+        }
+        for(let i = i_start; i < this.name.length; i++){
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                i_end = i;
+                break;
+            }
+        }
+        return this.name.substring(i_start, i_end);
     }
 
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation");
+        let ci = 0;
+        let i_start = 0; // incl.
+        let i_end = this.name.length;  // excl.
+        for(let i = 0; i < this.name.length; i++){
+            if(ci === n){
+                i_start = i;
+                break;
+            }
+
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                ci++;
+            }
+        }
+        for(let i = i_start; i < this.name.length; i++){
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                i_end = i;
+                break;
+            }
+        }
+        this.name = this.name.substring(0, i_start) + c + this.name.substring(i_end);
     }
 
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation");
+        let ci = 0;
+        let i_start = 0; // incl.
+        let i_end = this.name.length;  // excl.
+        for(let i = 0; i < this.name.length; i++){
+            if(ci === n){
+                i_start = i;
+                break;
+            }
+
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                ci++;
+            }
+        }
+        for(let i = i_start; i < this.name.length; i++){
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                i_end = i;
+                break;
+            }
+        }
+        this.name = this.name.substring(0, i_start) + c + this.delimiter + this.name.substring(i_start);
+        this.length++;
     }
 
     public append(c: string): void {
-        throw new Error("needs implementation");
+        // da leerer String bereits eine Komponente besitzt kein check auf isEmpty
+        this.name = this.name.concat(this.delimiter, c);
+        this.length++;
     }
 
     public remove(n: number): void {
-        throw new Error("needs implementation");
+        let ci = 0;
+        let i_start = 0; // incl.
+        let i_end = this.name.length;  // excl.
+        for(let i = 0; i < this.name.length; i++){
+            if(ci === n){
+                i_start = i;
+                break;
+            }
+
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                ci++;
+            }
+        }
+        for(let i = i_start; i < this.name.length; i++){
+            if(this.name.charAt(i) === this.delimiter && this.name.charAt(i-1) !== ESCAPE_CHARACTER){
+                i_end = i;
+                break;
+            }
+        }
+        this.name = this.name.substring(0, i_start) + this.name.substring(i_end+1);
+        this.length--;
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation");
+        if(this.delimiter !== other.getDelimiterCharacter()) return // https://www.studon.fau.de/studon/ilias.php?ref_id=4447999&cmdClass=ilobjforumgui&pos_pk=1130228&thr_pk=385173&page=0&viewmode=2&cmd=markPostRead&cmdNode=13z:tp&baseClass=ilRepositoryGUI#1130228
+        for(let i = 0; i < other.getNoComponents(); i++){
+            this.append(other.getComponent(i));
+        }
+    }
+
+    /** replace delimiter ignoring escaped delimiter characters */
+    protected replaceDelimiter(delimiter: string): string {
+        if(delimiter === this.delimiter) return this.name;
+
+        let out: string = "";
+        let i = 0;
+        while(i < this.name.length){
+            let c: string = this.name.charAt(i);
+
+            // remove unnecessary escape characters
+            if(c === ESCAPE_CHARACTER && this.name.charAt(i+1) === this.delimiter){
+                out += this.delimiter;
+                i += 2;
+                continue;
+            }
+
+            // replace delimiter
+            if(c === this.delimiter){
+                c = delimiter;
+            }
+
+            out += c;
+            i++;
+        }
+        return out;
     }
 
 }
